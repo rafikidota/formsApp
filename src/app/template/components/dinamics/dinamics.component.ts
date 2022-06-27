97fa546415f3992d97f7e98d25aa4adc187bacca
rafikidota <lesmes240698@gmail.com>
2022-06-27 09:12:20 -0400

    dinamic template forms

--- src/app/template/components/dinamics/dinamics.component.ts
+++ src/app/template/components/dinamics/dinamics.component.ts
@@ -1,14 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
+
+
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html'
 })
 export class DinamicsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  newFav: string = '';
+  person: Person = {
+    name: 'David',
+    favorites: [
+      { id: 1, name: 'Metal Gear' },
+      { id: 2, name: 'DeathStranding' }
+    ],
   }
 
+  constructor() { }
+  ngOnInit(): void { }
+
+  save() {
+    console.log('post successfully');
+  }
+  delete(index: number) {
+    this.person.favorites.splice(index, 1);
+  }
+  add() {
+    const id = this.getNewID();
+    const fav: Favorite = {
+      id: id,
+      name: this.newFav
+    }
+    this.person.favorites.push(fav);
+    this.newFav = '';
+  }
+  getNewID() {
+    let lastID = 0;
+    this.person.favorites.forEach(fav => {
+      if (fav.id > lastID) {
+        lastID = fav.id;
+      }
+    });
+    lastID++;
+    return lastID;
+  }
 }
