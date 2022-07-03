ac2c6d8e74d0512a598470a1924d94a0941641c7
rafikidota <lesmes240698@gmail.com>
2022-07-03 16:07:13 -0400

    section 19

--- src/app/selector/pages/selector/selector.component.ts
+++ src/app/selector/pages/selector/selector.component.ts
@@ -12,37 +12,73 @@ export class SelectorComponent implements OnInit {
 
   myForm: FormGroup = this.fb.group({
     region: ['', [Validators.required], []],
-    country: ['', [Validators.required], []]
+    country: ['', [Validators.required], []],
+    border: ['', [Validators.required], []]
   });
+
+  //Selectores
   regions: string[] = this.cs.regiones;
   countries: SmallCountry[] = [];
+  borders: SmallCountry[] = [];
+
+  //UI
+  loading: boolean = false;
 
   get regionIsEmpty() {
     return this.myForm.get('region')?.value === '';
+  }
+  get countryIsEmpty() {
+    return this.myForm.get('country')?.value === '';
   }
   constructor(private fb: FormBuilder,
     private cs: CountryService) { }
 
   ngOnInit(): void {
 
+    //Cuando cambia la región
     this.myForm.get('region')?.valueChanges
       .pipe(
-        tap((_) => {
+        tap(() => {
+          this.loading = true;
+          this.countries = [];
           this.myForm.get('country')?.reset('');
-          if (this.regionIsEmpty) {
-            this.countries = [];
-          }
         }),
         switchMap(region => {
           if (this.regionIsEmpty) {
-            return []
+            return [];
           }
-          return this.cs.getCountriesByRegion(region)
-        }
-        )
+          return this.cs.getCountriesByRegion(region);
+        })
       ).subscribe(countries => {
-        this.countries = countries;
+        this.loading = false;
+        this.countries = this.sort(countries);
       });
+
+    //Cuando cambia el país
+    this.myForm.get('country')?.valueChanges
+      .pipe(
+        tap(() => {
+          this.loading = true;
+          this.borders = [];
+          this.myForm.get('border')?.reset('');
+        }),
+        switchMap(cca3 => {
+
+          if (this.countryIsEmpty) {
+            return [];
+          }
+          return this.cs.getCountryByCCA3(cca3);
+        }),
+        switchMap(country => {
+          return this.cs.getCountriesyByCCA3Small(country[0].borders)
+        })
+      )
+      .subscribe(borders => {
+        //this.borders = country[0].borders || [];
+        this.borders = this.sort(borders);
+        this.loading = false;
+      });
+
 
   }
 
@@ -50,8 +86,15 @@ export class SelectorComponent implements OnInit {
     console.log(this.myForm.value);
   }
 
+  sort(countries: SmallCountry[]) {
+    
+    function SortArray(x: SmallCountry, y: SmallCountry) {
+      if (x.name.common < y.name.common) { return -1; }
+      if (x.name.common > y.name.common) { return 1; }
+      return 0;
+    }
+    
+    countries = countries.sort(SortArray);  
+    return countries;
+  }
 }
-function _(_: any): import("rxjs").OperatorFunction<any, any> {
-  throw new Error('Function not implemented.');
-}
-
--- src/app/selector/services/country.service.ts
+++ src/app/selector/services/country.service.ts
@@ -1,7 +1,8 @@
 import { HttpParams, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { SmallCountry } from '../interfaces/country.interface';
+import { Country, SmallCountry } from '../interfaces/country.interface';
+import { combineLatest, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,44 @@ export class CountryService {
     return [...this._regions];
   }
   get httpParams() {
-    //return new HttpParams().set('fields', 'name,capital,flags,cca2,population');
     return new HttpParams().set('fields', 'name,cca3');
   }
-  getCountriesByRegion(query: string) {
-    const url = `${this._rc}/region/${query}/`;
-
+  getCountriesByRegion(query: string): Observable<SmallCountry[]> {
+    const url = `${this._rc}/region/${query}`;
     return this.http.get<SmallCountry[]>(url, { params: this.httpParams });
   }
+  getCountryByCCA3(query: string): Observable<Country[]> {
+    const url = `${this._rc}/alpha/${query}`;
+    return this.http.get<Country[]>(url);
+  }
+  getCountryByCCA3Small(query: string): Observable<SmallCountry[]> {
+    const url = `${this._rc}/alpha/${query}`;
+    return this.http.get<SmallCountry[]>(url, { params: this.httpParams });
+  }
+
+  // Este es el metodo de Fernando Herrera
+  // getCountriesyByCCA3Small(query: string[]): Observable<SmallCountry[]> {
+  //   if (!query) {
+  //     return of([]);
+  //   }
+  //   const requests: Observable<SmallCountry>[]=[];
+  //   query.forEach(cca3 => {
+  //     const request = this.getCountryByCCA3Small(cca3);
+  //     requests.push(request); //error, esta linea no pincha
+  //   });
+  //   return combineLatest(requests);
+  // }
+
+  getCountriesyByCCA3Small(query: string[]): Observable<SmallCountry[]> {
+    if (!query) {
+      return of([]);
+    }
+    const codes = query.join();
+    const httpParams = new HttpParams().set('codes', codes).set('fields', 'name,cca3');
+    const url = `${this._rc}/alpha`;
+    return this.http.get<SmallCountry[]>(url, { params: httpParams });
+  }
+
 
   constructor(private http: HttpClient) { }
 }
